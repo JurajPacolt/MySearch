af79a10c067d027ecea86885c4e8a1bad2327ea1
Juraj Pacolt <JurajPacolt@github.com>
2021-12-28 23:10:09 +0100

    Start up ...

---
+++ src/main/java/org/javerland/mysearch/MySearchApplication.java
@@ -0,0 +1,13 @@
+package org.javerland.mysearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/javerland/mysearch/MySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javerland.mysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
